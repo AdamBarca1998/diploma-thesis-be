04da5a1b1a5b780196f8203f9e1635228f4f3093
Adam Barča <adambarca333@gmail.com>
2023-10-17 00:08:42 +0200

    INIT like modules

---
+++ calculator-server/src/test/java/sk/adambarca/calculatorserver/CalculatorServerApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.adambarca.calculatorserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
