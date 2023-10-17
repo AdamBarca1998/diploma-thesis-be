d7ab4389033dd9882586e12c67ca743ed98537f1
barca <xbarca04@stud.fit.vutbr.cz>
2023-10-17 11:30:51 +0200

    IMPL controller for calculator

--- calculator-server/src/main/java/sk/adambarca/calculatorserver/CalculatorServerApplication.java
+++ calculator-server/src/main/java/sk/adambarca/calculatorserver/CalculatorServerApplication.java
@@ -2,8 +2,9 @@ package sk.adambarca.calculatorserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class CalculatorServerApplication {
 
     public static void main(String[] args) {
