234ed823e49e021e2f83a051d90b468eb84aeb42
barca <xbarca04@stud.fit.vutbr.cz>
2023-10-27 11:55:20 +0200

    REFACT

--- calculator-server/src/main/java/sk/adambarca/calculatorserver/CalculatorMResource.java
+++ calculator-server/src/main/java/sk/adambarca/calculatorserver/CalculatorMResource.java
@@ -1,15 +1,15 @@
 package sk.adambarca.calculatorserver;
 
-import sk.adambarca.managementframework.annotation.MResource;
+import sk.adambarca.managementframework.resource.MResource;
 
 import java.util.Arrays;
 
 @MResource
 public final class CalculatorMResource {
 
-    public Double result = null;
+    public Double result = 0.1;
 
-    public double addAll(double[] numbers) {
+    public double sumAll(double[] numbers) {
         return Arrays.stream(numbers).sum();
     }
 }
